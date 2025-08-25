9050d4bbd76b678bc7614e0fa19724d5890e9410
Nicolas <ednip7@gmail.com>
2025-08-25 13:03:35 -0300

    Modified serial reading to be non blocking

--- src/libnmea_navsat_driver/nodes/nmea_serial_driver.py
+++ src/libnmea_navsat_driver/nodes/nmea_serial_driver.py
@@ -47,23 +47,30 @@ def main(args=None):
     serial_baud = driver.declare_parameter('baud', 460800).value
 
     try:
-        GPS = serial.Serial(port=serial_port, baudrate=serial_baud, timeout=2)
+        # timeout=0 → non-blocking
+        GPS = serial.Serial(port=serial_port, baudrate=serial_baud, timeout=0)
         driver.get_logger().info("Successfully connected to {0} at {1}.".format(serial_port, serial_baud))
+
         try:
             while rclpy.ok():
                 data = GPS.readline().strip()
-                try:
-                    if isinstance(data, bytes):
-                        data = data.decode("utf-8")
-                    driver.add_sentence(data, frame_id)
-                except ValueError as e:
-                    driver.get_logger().warn(
-                        "Value error, likely due to missing fields in the NMEA message. Error was: %s. "
-                        "Please report this issue at github.com/ros-drivers/nmea_navsat_driver, including a bag file "
-                        "with the NMEA sentences that caused it." % e)
+
+                if data:  # Only process if there is something
+                    try:
+                        if isinstance(data, bytes):
+                            data = data.decode("utf-8")
+                        driver.add_sentence(data, frame_id)
+                    except ValueError as e:
+                        driver.get_logger().warn(
+                            "Value error, likely due to missing fields in the NMEA message. Error was: %s. "
+                            "Please report this issue at github.com/ros-drivers/nmea_navsat_driver, including a bag file "
+                            "with the NMEA sentences that caused it." % e)
+
+                time.sleep(0.01)
 
         except Exception as e:
             driver.get_logger().error("Ros error: {0}".format(e))
             GPS.close()  # Close GPS serial port
+
     except serial.SerialException as ex:
         driver.get_logger().fatal("Could not open serial port: I/O error({0}): {1}".format(ex.errno, ex.strerror))
